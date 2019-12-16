641e4679c6277408664d2cabec2f9d4cecc93315
Bugra Onal <onalbugra@gmail.com>
2019-12-16 22:41:19 +0300

    Downscale python buffers added

--- SW/downscalce.py
+++ SW/downscalce.py
@@ -12,36 +12,57 @@ if (len(sys.argv) > 1):
 else:
 	img = cv2.imread("test.jpg", cv2.IMREAD_GRAYSCALE)
 
-rows = 9 
-cols = 8
+print(img.shape)
+rows = 200 
+cols = 200
 
 out = np.zeros((rows,cols), dtype=np.int)
-out_count = np.zeros((rows,cols), dtype=np.int)
+#out_count = np.zeros((rows,cols), dtype=np.int)
 factor_rows = rows / img.shape[0]
 factor_cols = cols / img.shape[1]
+
+buff_u = 0
+buff_v = 0
+
+buff_00 = 0
+buff_01 = 0
+buff_10 = 0
+buff_11 = 0
 
 for i in range(img.shape[0]):
 	for j in range(img.shape[1]):
 		pix = img[i][j]
 		u = (i * factor_rows)
 		v = (j * factor_cols)
+		if (int(u) != buff_u or int(v) != buff_v):
+			out[buff_u][buff_v] 		= buff_00
+			out[buff_u][buff_v + 1] 	= buff_01
+			out[buff_u + 1][buff_v] 	= buff_10
+			out[buff_u + 1][buff_v + 1] = buff_11
+
+			buff_u = min(int(u), rows - 2)
+			buff_v = min(int(v), cols - 2)
+			buff_00 = out[buff_u][buff_v]
+			buff_01 = out[buff_u][buff_v + 1]
+			buff_10 = out[buff_u + 1][buff_v]
+			buff_11 = out[buff_u + 1][buff_v + 1]
 		du = u - int(u)
 		dv = v - int(v)
 
-		out[int(u)]		[int(v)] 		+= (1 - du) * (1 - dv) * pix
-		out_count[int(u)]		[int(v)] 		+= 1 
+		buff_00 		+= (1 - du) * (1 - dv) * pix
+		#out_count[int(u)][int(v)] 		+= 1 
 		if (u + 1 < rows):
-			out_count[int(u + 1)]	[int(v)] 		+= 1
-			out[int(u + 1)]	[int(v)] 		+= du * (1 - dv) * pix
+		#	out_count[int(u + 1)]	[int(v)] 		+= 1
+			buff_10 	+= du * (1 - dv) * pix
 		if (v + 1 < cols):
-			out[int(u)]		[int(v + 1)] 	+= (1 - du) * dv * pix
-			out_count[int(u)]		[int(v + 1)] 	+= 1
+			buff_01 	+= (1 - du) * dv * pix
+		#	out_count[int(u)][int(v + 1)] 	+= 1
 		if (u + 1 < rows  and v + 1 < cols):
-			out[int(u + 1)]	[int(v + 1)] 	+= du * dv * pix
-			out_count[int(u + 1)]	[int(v + 1)] 	+= 1
+			buff_11 	+= du * dv * pix
+		#	out_count[int(u + 1)][int(v + 1)] 	+= 1
 
-out = (out / np.mean(out_count) * 4).astype("uint8")
-print(np.mean(out_count))
+out = (out / 236 * 4).astype("uint8")
+#print((out_count))
 
 cv2.imshow("Original", img)
 cv2.imshow("Downscaled", out)
